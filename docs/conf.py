6df394620dac92b71aebb5898ab4884bcda84fa0
bhargavyagnik <bhargavyagnik99@gmail.com>
2024-11-04 00:09:43 -0500

    updates to conf

--- docs/conf.py
+++ docs/conf.py
@@ -1,32 +1,29 @@
 import os
 import sys
 
-import sphinx.ext.pygments
-sphinx.ext.pygments.lexer_mapping['console'] = 'posix-terminal'
+# MyST Markdown parser settings
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "fieldlist",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "tasklist",
+]
 
 project = 'Generative AI Python'
 copyright = '2024, Your Name'
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'myst_parser',
-    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
 ]
 
 source_suffix = '.md'
 master_doc = 'index'  # Set the homepage
-
-extensions = ['myst_nb']
 # Configure autodoc to use markdown
-autodoc_default_flags = ['members', 'undoc-members', 'show-inheritance']
-autodoc_member_order = 'bysource'
-
-# Configure markdown processing
-napoleon_use_param = True  # Enable napoleon for docstrings
 
 # Configure HTML output
 html_theme = 'pydata_sphinx_theme'  # Choose a theme (optional)
@@ -46,16 +43,7 @@ html_theme_options = {
     'collapse_navigation': False,  # Show navigation by default
 }
 
-# MyST Markdown parser settings
-myst_enable_extensions = [
-    "colon_fence",
-    "deflist",
-    "fieldlist",
-    "html_image",
-    "replacements",
-    "smartquotes",
-    "tasklist",
-]
+
 
 
 def setup(app):
